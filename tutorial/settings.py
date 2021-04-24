7e122509daafb0983950002398951730097f8f51
po1nt <po1nt@protonmail.ch>
2021-04-24 17:22:25 +1000

    sec_update_2

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -1,18 +1,13 @@
 
 
-from pathlib import Path
-
 import os
+from pathlib import Path
 
 try:
     import django_heroku
 except:
     django_heroku = None
 
-try:
-    import gunicorn
-except:
-    gunicorn = None
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.os.environ['dj_SECRET_KEY']
+SECRET_KEY = os.environ['dj_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
