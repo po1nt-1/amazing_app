cea3d6c6c35eb930f5ae887996b1b026e5cde7db
po1nt <po1nt@protonmail.ch>
2021-04-25 02:13:55 +1000

    try to deploy

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework_extensions.routers import ExtendedDefaultRouter
@@ -31,4 +29,4 @@ urlpatterns = [
     path('v1/', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
